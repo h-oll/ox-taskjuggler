9d5aa1ed13b3e1874c828b72f9a8c76d8c08ef5e
holl@potassium <harold.ollivier@inria.fr>
2026-04-02 22:28:54 +0200

    changing the default exported keywords

--- lisp/ox-taskjuggler.el
+++ lisp/ox-taskjuggler.el
@@ -335,7 +335,7 @@ list."
 
 (defcustom org-taskjuggler-valid-resource-attributes
   '(limits vacation shift booking efficiency journalentry rate
-	   workinghours flags chargeset)
+	   workinghours flags chargeset leaves)
   "Valid attributes for Taskjuggler resources.
 If one of these appears as a property for a headline, it will be
 exported with the corresponding resource."
